43a8e16719657fef0d68ae52296ee84f835112f9
felipemfm <felipemfm@gmail.com>
2022-08-22 11:28:26 +0900

    Feat: Set up nodemon, morgan, cors dev dependencies

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env" });
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  const morgan = require("morgan");
+  const cors = require("cors");
+  app.use(morgan("dev"));
+  app.use(cors());
+}
+
+app.use(express.json());
 
 module.exports = app;
